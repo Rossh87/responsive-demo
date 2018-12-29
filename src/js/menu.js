f1d0d4ff20c2b79932d09428d946ca3a7b758193
Ross Hunter <rossh.87@gmail.com>
2018-12-29 02:53:46 -0600

    correctly expands nav menu at large screen sizes

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,5 +1,5 @@
-const menuButton = document.querySelector('.nav-toggle');
-const menuModal = document.querySelector('.main-nav');
+const menuButton = document.querySelector('.header_nav_toggle');
+const menuModal = document.querySelector('.header_nav');
 const hamburger = document.querySelector('.hamburger');
 
 menuButton.addEventListener('click', e=> {
